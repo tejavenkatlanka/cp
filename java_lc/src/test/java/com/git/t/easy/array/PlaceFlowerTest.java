54ba96d200e36d78f67c15e469a637a9caba929a
tejavenkat lanka <tvlanka9@gmail.com>
2020-12-02 00:22:59 +0530

    adjacent insert and right side vide

---
+++ java_lc/src/test/java/com/git/t/easy/array/PlaceFlowerTest.java
@@ -0,0 +1,17 @@
+package com.git.t.easy.array;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class PlaceFlowerTest {
+
+  @Test
+  void canPlaceFlowers() {
+    PlaceFlower placeFlower=new PlaceFlower();
+    assertTrue(placeFlower.canPlaceFlowers(new int[]{1,0,0,0,1},1));
+    assertFalse(placeFlower.canPlaceFlowers(new int[]{1,0,0,0,1},2));
+    assertFalse(placeFlower.canPlaceFlowers(new int[]{1,0,0,0,0,1}, 2));
+  }
+}
---
+++ java_lc/src/test/java/com/git/t/medium/Tree/RightSideViewTest.java
@@ -0,0 +1,27 @@
+package com.git.t.medium.Tree;
+
+import com.git.t.common.TreeNode;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RightSideViewTest {
+
+  @Test
+  void rightSideView() {
+    RightSideView rightSIdeVIew=new RightSideView();
+    Assertions.assertEquals(List.of(1),rightSIdeVIew.rightSideView(new TreeNode(1)));
+
+    TreeNode root = new TreeNode(1);
+    root.right=new TreeNode(2);
+    Assertions.assertEquals(List.of(1,2),rightSIdeVIew.rightSideView(root));
+    rightSideViewWithLeftSideChild();
+  }
+  @Test
+  void rightSideViewWithLeftSideChild() {
+    RightSideView rightSIdeVIew = new RightSideView();;
+    TreeNode root = new TreeNode(1);
+    root.left=new TreeNode(2);
+    Assertions.assertEquals(List.of(1,2),rightSIdeVIew.rightSideView(root));
+  }
+}
