9f74329e4fcfc986366b018d4ed69ca27bb3e936
tejavenkat lanka <tvlanka9@gmail.com>
2020-12-03 21:12:49 +0530

    min sub array

---
+++ java_lc/src/test/java/com/git/t/medium/arrary/MinSubArrayTest.java
@@ -0,0 +1,14 @@
+package com.git.t.medium.arrary;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MinSubArrayTest {
+
+  @Test
+  void minSubArrayLen() {
+    MinSubArray minSubArray=new MinSubArray();
+    assertEquals(2,minSubArray.minSubArrayLen(7,new int[]{2,3,1,2,4,3}));
+  }
+}
